d3f48f6b75d68f3488549a9fc16023f88e19bacd
OSHIN <izkp44@gmail.com>
2023-03-01 23:10:37 +0700

    Update start.js

--- 8-what-is-this/start.js
+++ 8-what-is-this/start.js
@@ -1,1 +1,19 @@
-;(() => {})()
+(() => {
+  // 1. Lexical scope & Dynamic scope
+
+  // function printName(params) {
+  //   console.log(this)
+  // }
+
+  // printName()
+
+  // 2. How to know what is "this"?
+  function printName(params) {
+    console.log(this)
+  }
+  // 2.1 Invoken object
+  const varayut = { name: 'Varayut', printName }
+  const jane = { name: 'Jane', printName }
+
+  jane.printName()
+})()
