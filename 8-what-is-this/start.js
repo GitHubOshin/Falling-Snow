698bb491abc5014c8ea0eb3293ce14d6fd063760
OSHIN <izkp44@gmail.com>
2023-03-02 02:36:51 +0700

    Update start.js

--- 8-what-is-this/start.js
+++ 8-what-is-this/start.js
@@ -10,10 +10,48 @@
   // 2. How to know what is "this"?
   function printName(params) {
     console.log(this)
+    console.log(`Maynameis ${this.name}`)
   }
   // 2.1 Invoken object
-  const varayut = { name: 'Varayut', printName }
-  const jane = { name: 'Jane', printName }
+  // const varayut = { name: 'Varayut', printName }
+  // const jane = { name: 'Jane', printName }
 
-  jane.printName()
+  // varayut.printName()
+  // jane.printName()
+
+  // 2.2 Global object (window, global)
+  // name = 'Global'
+  // printName()
+
+  // 2.3 Constructor function
+  // function Person(name) {
+  //   this.name = name
+  //   this.printName = printName
+  // }
+
+  // const varayut = new Person('Varayut')
+  // varayut.printName()
+
+  // 3. call(), apply() and bind()
+  function printName(nationality, city) {
+    console.log(this)
+    console.log(
+      `My name is ${this.name},  I'm ${nationality} and am living in ${city}`
+    )
+  }
+
+  function Person(name, nationality, city) {
+    this.name = name
+    this.nationality = nationality
+    this.city = city
+
+    printName(this.nationality, this.city)
+    printName.call(this, this.nationality, this.city)
+    printName.apply(this, [this.nationality, this.city])
+
+    const printVarayut = printName.bind(this)
+    printVarayut(this.nationality, this.city)
+  }
+
+  const varayut = new Person('Varayut', 'Thai', 'Bangkok')
 })()
